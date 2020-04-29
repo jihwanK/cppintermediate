861bbbc3ba4f2c0986c3a40821471157a7816b81
jihwanK <jhwan1228@gmail.com>
2020-04-29 16:08:14 +0900

    project 3

---
+++ project1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	//vector<int> x {1,2,3,4,5,6,7,8,9,10};
+	int x[10]{1,2,3,4,5,6,7,8,9,10};
+	int y[10];
+	int v1 = 10;
+
+	transform ( /* lambda expression */ );
+
+	for ( auto n : y)
+		cout << n << endl;
+}
